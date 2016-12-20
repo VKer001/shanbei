ced4e72a15e3ea1454808bbbb64a3369f7de31a1
VKer001 <820409748@qq.com>
2016-12-20 22:13:36 +0800

    12.20

--- js/shanbei.js
+++ js/shanbei.js
@@ -21,7 +21,7 @@ var playNext = function() {
     play(1)
 }
 
-
+// 左右 turn 事件
 var bindTurn = function() {
     $('.vker-slide-turn').on('click', function() {
         if ($(this).hasClass('vker-slide-turn-left')) {
@@ -32,8 +32,39 @@ var bindTurn = function() {
     })
 }
 
+// 指示器 事件
+var bindIndicators = function() {
+    $('.vker-slide-i').on('click mouseover', function() {
+        var i = $(this).index()
+        console.log(i);
+        $('.vker-slide-active').removeClass('vker-slide-active')
+        $($('.vker-slide-img')[i]).addClass('vker-slide-active')
+
+        $('.vker-slide-i-active').removeClass('vker-slide-i-active')
+        $($('.vker-slide-i')[i]).addClass('vker-slide-i-active')
+    })
+}
+
+// 轮播延时设置
+var timeTurn = function() {
+    var main = $('.vker-slide-img')
+    var timeId = setInterval(playNext, 2000)
+    main.data('timeId', timeId)
+ 
+    main.on('mouseover', function() {
+        var timeId = main.data('timeId')
+        clearInterval(timeId)
+    })
+    main.on('mouseout', function() {
+        var timeId = setInterval(playNext, 2000)
+        main.data('timeId', timeId)
+    })
+}
+
 var _main = function() {
     bindTurn()
+    bindIndicators()
+    timeTurn()
 }
 
 _main()
